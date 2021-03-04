a0ca2f2d6f2254429ac30b2c02b86d0a05d97daa
Wander <wander@bigbangcomunicacao.com.br>
2021-03-03 21:33:10 -0300

    feat: added operator

---
+++ 6-operator/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//arithmetic
+	sum := 1 + 2
+	subtraction := 1 - 2
+	division := 10 / 4
+	multiplication := 10 * 5
+	restDivision := 10 % 2
+
+	fmt.Println(sum, subtraction, division, multiplication, restDivision)
+
+}
