83155679bb6bb3ff49c28a18720a6e6dad5423b2
Wander <wander@bigbangcomunicacao.com.br>
2021-03-08 21:07:28 -0300

    feat: added switch

---
+++ 13-switch/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Switch")
+
+	day := dayOfWeek(2)
+	fmt.Println(day)
+}
+
+func dayOfWeek(number int) string {
+	switch number {
+	case 1:
+		return "Sunday"
+	case 2:
+		return "Monday"
+	case 3:
+		return "Tuesday"
+	case 4:
+		return "Wednesday"
+	case 5:
+		return "Thursday"
+	case 6:
+		return "Friday"
+	case 7:
+		return "Saturday"
+	default:
+		return "Invalid number"
+	}
+}
